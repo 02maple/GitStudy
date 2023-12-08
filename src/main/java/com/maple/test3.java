46d5109da37cdcda40302de1d7d66d06b56be03c
maple <fyezsj@163.com>
2023-12-08 10:16:24 +0800

    test1

---
+++ src/main/java/com/maple/test3.java
@@ -0,0 +1,8 @@
+package com.maple;
+
+public class test3 {
+    public static void main(String[] args) {
+        System.out.println("2023年9月7日23点22分");
+        System.out.println("第一次创建test1分支测试");
+    }
+}
